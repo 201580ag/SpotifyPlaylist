ca868062262beec6f26e1c8202591cfffadee26a
201580ag <39054577+201580ag@users.noreply.github.com>
2024-10-19 10:17:30 +0900

    Update CoverImageChanger.py

--- TEST/CoverImageChanger.py
+++ TEST/CoverImageChanger.py
@@ -1,3 +1,35 @@
+# Project: Spotify Playlist Downloader
+# Version: 1.0
+# Author: 201580ag
+# Description:
+#   This project downloads playlist information from Spotify using its API,
+#   retrieves the songs from YouTube, converts them to MP3, and adds metadata
+#   and cover images to the MP3 files.
+#
+# Resources:
+# - GitHub Repository: https://github.com/201580ag/SpotifyPlaylist
+# - Documentation: docs/README.md
+# - Issues & Support: https://github.com/201580ag/SpotifyPlaylist/issues
+#
+# Developed by 201580ag and contributors.
+# See LICENSE.txt for copyright and licensing details (MIT License).
+#
+# Disclaimer:
+#   This software is provided "as is", without warranty of any kind, express or implied,
+#   including but not limited to the warranties of merchantability, fitness for a
+#   particular purpose and noninfringement. In no event shall the authors or copyright
+#   holders be liable for any claim, damages or other liability, whether in an action
+#   of contract, tort or otherwise, arising from, out of or in connection with the
+#   software or the use or other dealings in the software.
+#
+#   By using this software, you acknowledge that you are solely responsible for any
+#   risks or issues that may arise, including but not limited to data loss, copyright
+#   infringement, or any other legal matters. The author assumes no responsibility for
+#   any misuse or consequences from using this software.
+#
+# This software is free and open-source. Support the project through contributions
+# or by reporting issues on GitHub. Contact: 201580ag@gmail.com
+
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, APIC, ID3NoHeaderError
 import os
